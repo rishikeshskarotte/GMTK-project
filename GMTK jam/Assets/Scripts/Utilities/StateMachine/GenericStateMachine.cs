dc83baae306c2b3f9c162fce63910cfde31c4664
Sarangu Baisil Sanal <sarangubaisil8281@gmail.com>
2025-07-31 09:05:55 +0530

    Scene setup

---
+++ GMTK jam/Assets/Scripts/Utilities/StateMachine/GenericStateMachine.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+namespace Utilities.StateMachine
+{
+    public class GenericStateMachine<T>
+    {
+        private IState<T> currentState;
+        private readonly T owner;
+        private Dictionary<Enum, IState<T>> states = new();
+        
+        public IState<T> CurrentState => currentState;
+        
+        public GenericStateMachine(T owner)
+        {
+            this.owner = owner;
+        }
+
+        protected void AddState(Enum state, IState<T> stateInstance)
+        {
+            states.TryAdd(state, stateInstance);
+        }
+
+        public void ChangeState(Enum key)
+        {
+            if (states.TryGetValue(key, out IState<T> newState))
+            {
+                if (currentState == newState) return;
+
+                currentState?.OnExit();
+                currentState = newState;
+                currentState?.OnEnter();
+            }
+        }
+
+        protected void SetOwner()
+        {
+            foreach (IState<T> state in states.Values)
+            {
+                state.Owner = owner;
+            }
+        }
+        
+        public void Tick()
+        {
+            currentState?.Tick();
+        }
+    }
+}
---
+++ GMTK jam/Assets/Scripts/Utilities/StateMachine/IState.cs
@@ -0,0 +1,11 @@
+namespace Utilities.StateMachine
+{
+    public interface IState<T>
+    {
+        public T Owner { get; set; }
+        public void OnEnter();
+        public void OnExit();
+        public void Tick();
+       
+    }
+}
